3e101eb21c50a4b080a09c04b4490986e41aec4a
Andrei Alexeev <andrei.alexeev1996@gmail.com>
2021-10-03 04:14:00 +0200

    add dev mode & add layer functions

--- server/controller/layers.js
+++ server/controller/layers.js
@@ -1,12 +1,53 @@
 const api = require("../helper/api");
-const { Layer, Image, ImageAttribute } = require("../../models/dbmodels");
+const { Layer, Image, ImageAttribute, db } = require("../../models/dbmodels");
 const { layersDir } = require("../../config");
-const { createIndex } = require("../helper/fsHelper");
+const { getIndex } = require("../helper/fsHelper");
 
 async function getLayers(req, res) {
   try {
     let layers = await Layer.findAll({ raw: true });
+    for (let layer = 0; layer < layers.length; layer++) {
+      layers[layer].size = await Image.count({ where: { layerId: layers[layer].id } });
+    }
     api.successResponseWithData(res, "OK", layers);
+  } catch (err) {
+    api.ErrorResponse(res, err.toString());
+  }
+}
+
+async function getImages(req, res) {
+  try {
+    let layerMap = new Map();
+    let layers = await Layer.findAll({ raw: true });
+    layers.forEach((layer) => {
+      layerMap.set(layer.id, layer.name);
+    });
+    let images = await Image.findAll({ raw: true });
+    images = images.map((image) => {
+      image.layername = layerMap.get(image.layerId);
+      return image;
+    });
+    api.successResponseWithData(res, "OK", images);
+  } catch (err) {
+    api.ErrorResponse(res, err.toString());
+  }
+}
+
+async function patchLayers(req, res, next) {
+  let layers = req.body.layers;
+  try {
+    if (layers) {
+      for (let layer = 0; layer < layers.length; layer++) {
+        await Layer.update(
+          { layerorder: layers[layer].layerorder },
+          { where: { id: layers[layer].id } }
+        );
+      }
+      next();
+      return;
+    } else {
+      api.BadRequestResponse(res, "Layers missing!");
+    }
   } catch (err) {
     api.ErrorResponse(res, err.toString());
   }
@@ -14,10 +55,10 @@ async function getLayers(req, res) {
 
 async function getLayersReload(req, res) {
   try {
-    let layers = createIndex(layersDir);
-    await Image.drop();
-    await Layer.drop();
-    await ImageAttribute.drop();
+    let layers = getIndex(layersDir);
+    await Image.destroy({ where: {} });
+    await Layer.destroy({ where: {} });
+    await ImageAttribute.destroy({ where: {} });
     layers.forEach(async (layer) => {
       let createdLayer = await Layer.create({ name: layer.name });
       layer.files.forEach(async (file) => {
@@ -38,7 +79,10 @@ async function getLayersReload(req, res) {
 async function getLayer(req, res) {
   let name = req.params.name;
   if (name) {
-    let layer = await Layer.findOne({ where: { name: name }, include: [Image], raw: true });
+    let layer = await Layer.findOne({
+      where: { name: name },
+      include: [{ model: Image, as: "images" }],
+    });
     api.successResponseWithData(res, "OK", layer);
   } else {
     api.BadRequestResponse(res, "No name for layer was given");
@@ -60,7 +104,7 @@ async function getTraitAttributes(req, res) {
   }
 }
 
-function postTraitAttributes(req, res) {
+async function postTraitAttributes(req, res) {
   let hash = req.params.hash;
   let layer = req.params.name;
   if (hash && layer) {
@@ -76,8 +120,9 @@ function postTraitAttributes(req, res) {
 module.exports = {
   getLayer,
   getLayers,
+  getImages,
+  patchLayers,
   getLayersReload,
-  getIndexIfNotExist,
   getTraitAttributes,
   postTraitAttributes,
 };
--- server/router/layers.js
+++ server/router/layers.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const layerController = require("../controller/layers");
 
-
-router.get("/", layerController.getIndexIfNotExist, layerController.getLayers);
-router.get("/reload", layerController.getLayersReload)
-router.get("/:name", layerController.getIndexIfNotExist, layerController.getLayer);
-router.get("/:name/:hash", layerController.getIndexIfNotExist, layerController.getTraitAttributes)
+router.get("/", layerController.getLayers);
+router.patch("/", layerController.patchLayers, layerController.getLayers)
+router.get("/images", layerController.getImages)
+router.get("/reload", layerController.getLayersReload);
+router.get("/:name", layerController.getLayer);
+router.get("/:name/:hash", layerController.getTraitAttributes);
 router.post("/:name/:hash", layerController.postTraitAttributes);
-
 
 module.exports = router;
