38cfcc20330c4258772bc0e4ce821ae35d0c5d73
Andrei Alexeev <andrei.alexeev1996@gmail.com>
2021-10-07 20:15:39 +0200

    update quick tests

--- tests/fsHelper.js
+++ tests/fsHelper.js
@@ -3,6 +3,6 @@ const { getIndex } = require("../server/helper/fsHelper");
 const layersDir = require("../config").layersDir;
 describe("FS Helper", function () {
   it("Get Index", function () {
-    console.log(getIndex(layersDir));
+    getIndex(layersDir);
   });
 });
